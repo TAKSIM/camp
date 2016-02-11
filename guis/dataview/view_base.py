b02ca2ea3d81e1c6b953c5f1e9d65180abbd6700
TAKSIM <wei_he_thu@163.com>
2016-02-11 18:46:33 +0800

    finished right click menu, fixed a bug in table view formatting. continue with data upload

--- guis/dataview/view_base.py
+++ guis/dataview/view_base.py
@@ -2,8 +2,29 @@
 from PyQt4 import QtGui, QtSql, QtCore
 
 
+class NumberDelegate(QtGui.QStyledItemDelegate):
+    def __init__(self, parent=None, withComma=True, numDigits=2):
+        QtGui.QStyledItemDelegate.__init__(self, parent)
+        self.withComma = withComma
+        self.numDigits = numDigits
+        self.strformat = withComma and '{:,.%sf}' % self.numDigits or ('{:.%sf}' % self.numDigits)
+
+    def displayText(self, QVariant, QLocale):
+        v, s = QVariant.toDouble()
+        return s and self.strformat.format(v) or 'NaN'
+
+
+class DateDelegate(QtGui.QStyledItemDelegate):
+    def __init__(self, parent=None):
+        QtGui.QStyledItemDelegate.__init__(self, parent)
+
+    def displayText(self, QVariant, QLocale):
+        v = QVariant.toDate()
+        return v.toString(QtCore.Qt.ISODate)
+
+
 class ViewBase(QtGui.QTableView):
-    def __init__(self, query, header, parent=None):
+    def __init__(self, query, header, menu=False, parent=None):
         QtGui.QTableView.__init__(self, parent)
         self.query = query
         self.header = header
@@ -26,6 +47,20 @@ class ViewBase(QtGui.QTableView):
         self.setSelectionBehavior(QtGui.QTableView.SelectRows)
 
 
+        if menu:
+            self.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+            self.customContextMenuRequested.connect(self.showRightClickMenu)
+            self.buildMenu()
+
+    def buildMenu(self):
+        raise NotImplemented()
+
+    def showRightClickMenu(self):
+        self.menu.exec_(QtGui.QCursor.pos())
+
+    def refresh(self):
+        self.dataModel.query().exec_()
+
 class ViewBaseSet:
     def __init__(self, vb, parent=None):
         self.vb = vb
@@ -37,10 +72,13 @@ class ViewBaseSet:
         self.sortContent = QtGui.QLineEdit()
         self.sortContent.textChanged.connect(self.filterRegExpChanged)
 
+        self.btnRefresh = QtGui.QPushButton(u'刷新')
+        self.btnRefresh.clicked.connect(self.vb.refresh)
+
     def filterRegExpChanged(self):
         syntax_nr = QtCore.QRegExp.FixedString
         syntax = QtCore.QRegExp.PatternSyntax(syntax_nr)
-        regExp = QtCore.QRegExp(self.sortCont.text(), QtCore.Qt.CaseInsensitive, syntax)
+        regExp = QtCore.QRegExp(self.sortContent.text(), QtCore.Qt.CaseInsensitive, syntax)
         self.vb.proxyModel.setFilterRegExp(regExp)
 
     def filterColumnChanged(self):
--- guis/dataview/view_subdetails.py
+++ guis/dataview/view_subdetails.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtGui, QtSql, QtCore
-from view_base import ViewBase, ViewBaseSet
+from view_base import ViewBase, ViewBaseSet, NumberDelegate, DateDelegate
+
 
 class LiabilityView(ViewBase):
     def __init__(self, parent=None):
@@ -21,21 +22,55 @@ class LiabilityView(ViewBase):
                     'left outer join sale_type s on s.ID=d.SALE_TYPE '
                     'left outer join expops_type e on e.id=d.EXP_OPS '
                     'left outer join client_type c on c.id=d.CLIENT_TYPE',
-            header = [u'封闭期到期日',
-                      u'到期本息',
-                      u'客户类型',
-                      u'委托人名称',
-                      u'销售类型',
-                      u'申请书编码',
-                      u'认购金额(元)',
-                      u'预期收益率(%)',
-                      u'认购日',
-                      u'封闭期起始日' ,
-                      u'期限',
-                      u'到期操作'],
-            parent = parent)
+            header = [u'封闭期到期日', # 0
+                      u'到期本息', # 1
+                      u'客户类型', # 2
+                      u'委托人名称', # 3
+                      u'销售类型', # 4
+                      u'申请书编码', # 5
+                      u'认购金额(元)', # 6
+                      u'预期收益率(%)', # 7
+                      u'认购日', # 8
+                      u'封闭期起始日' , # 9
+                      u'期限', # 10
+                      u'到期操作'], # 11
+            menu    = True,
+            parent  = parent)
 
         self.sortByColumn(0, QtCore.Qt.DescendingOrder)
+        nfAmt = NumberDelegate(parent=self, withComma=True, numDigits=0)
+        nfPct = NumberDelegate(parent=self, withComma=False, numDigits=2)
+        df = DateDelegate(parent=self)
+        self.setItemDelegateForColumn(0, df)
+        self.setItemDelegateForColumn(1, nfAmt)
+        self.setItemDelegateForColumn(6, nfAmt)
+        self.setItemDelegateForColumn(7, nfPct)
+        self.setItemDelegateForColumn(8, df)
+        self.setItemDelegateForColumn(9, df)
+
+    def buildMenu(self):
+        self.menu = QtGui.QMenu()
+        actRemoveRow = QtGui.QAction(u'删除本条记录', self, triggered=self.removeRow)
+        self.menu.addAction(actRemoveRow)
+
+    def removeRow(self):
+        rowIndex = self.currentIndex().row()
+        subcode = self.model().index(rowIndex, 5).data().toString()
+        reply = QtGui.QMessageBox.question(self, u'删除本条记录',
+                                           u'申请书编号：%s'% subcode,
+                                           QtGui.QMessageBox.Yes | QtGui.QMessageBox.No,
+                                           QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            q = QtSql.QSqlQuery()
+            try:
+                q.exec_("""DELETE FROM LIABILITY WHERE SUB_CODE='%s'""" % subcode)
+                QtSql.QSqlDatabase().commit()
+                self.model().removeRow(rowIndex)
+                self.refresh()
+            except Exception, e:
+                print e.message
+                QtSql.QSqlDatabase().rollback()
+
 
 
 class LiabilityViewSet(ViewBaseSet):
